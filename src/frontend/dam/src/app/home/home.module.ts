8f7de284a312d5522459b73b3bdb8167f5150923
Javier Fanelli <javifanelli@gmail.com>
2023-04-23 08:12:52 -0300

    commit menor. no anda

--- src/frontend/dam/src/app/home/home.module.ts
+++ src/frontend/dam/src/app/home/home.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
 
 
@@ -13,8 +12,9 @@ import { HomePageRoutingModule } from './home-routing.module';
     FormsModule,
     IonicModule,
     HomePageRoutingModule,
-    HomePage
+    
   ],
+  declarations: [HomePage]
 })
 
 export class HomePageModule {}
--- src/frontend/dam/src/app/services/dispositivo.service.ts
+++ src/frontend/dam/src/app/services/dispositivo.service.ts
@@ -8,11 +8,58 @@ import { Riegos } from '../interfaces/riegos';
 @Injectable({
   providedIn: 'root'
 })
+
 export class DispositivoService {
   private URLServer = 'http://localhost:8000';
   constructor(private _http: HttpClient) {}
 
   getListaDisp (): Observable<Dispositivo[]> {
-    return this._http.get<Dispositivo[]>(this.URLServer + '/devices');
+    return this._http.get<Dispositivo[]>(this.URLServer + '/dispositivos');
   }
+
+  public getUltMedicion(id: number): Observable<Medicion[]> {
+    return this._http.get<Medicion[]>(this.URLServer + '/ultmedicion/'+id);
+  }
+
+  postLogRiegos(deviceId: number) {
+    let logRiego: Riegos = {
+      apertura: '1',
+      fecha: (new Date()).toISOString().split('T')[0],
+      electrovalvulaId: deviceId
+    }
+    this._http.post(this.URLServer + '/logriegos',logRiego)
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+
+  public postMedicion(dispositivoId: number, valorMedicion: string) {
+    let medicion: Medicion = {
+      fecha: (new Date()).toISOString().split('T')[0],
+      valor: valorMedicion,
+      dispositivoId: dispositivoId
+    }
+    this._http.post(this.URLServer + '/mediciones', medicion)
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+
+  public getMediciones(id: number): Observable<Medicion[]> {
+    return this._http.get<Medicion[]>(this.URLServer + '/dispositivos/'+id+'/mediciones');
+  }
+
+  public getLogRiegos(electrovalvulaId: number): Observable<Riegos[]> {
+    return this._http.get<Riegos[]>(this.URLServer + '/logriegos/'+electrovalvulaId);
+  }
+
+  public abrirElectrovalvula(dispositivoId: number) {
+    this.postLogRiegos(dispositivoId);
+    this.postMedicion(dispositivoId, '0');
+  }
+
+  public getDeviceById(id: number): Observable<Dispositivo[]> {
+    return this._http.get<Dispositivo[]>(this.URLServer + '/dispositivos/'+id);
+  }
+
 }
