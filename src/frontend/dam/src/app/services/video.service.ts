1a347a0aad9757bfef6e321d2bc0f3082a91b7ac
javifanelli <javifanelli@gmail.com>
2023-12-19 22:57:20 -0300

    algo de video y clima

---
+++ src/frontend/dam/src/app/services/video.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+  })
+  export class VideoService {
+    private URLServer = 'http://192.168.0.70:8000';
+    constructor(private _http: HttpClient) {}
+
+    getVideoData(): Observable<any[]> {
+        return this._http.get<any[]>('../assets/video.json');
+      }
+  }
